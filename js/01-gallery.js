a243ef5c5a6d267662975c1aa3d79486edaf518f
Mykola21 <kolasickotov@gmail.com>
2023-05-25 15:35:08 +0300

    error

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector('ul.gallery');
+
+function createElemListImg (items) {
+    return galleryItems
+    .map(({ preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="Image Description"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+galleryList.insertAdjacentHTML('beforeend', createElemListImg(galleryItems));
+galleryList.addEventListener('click', onGaleryClick);
+
+function onGaleryClick(evt) {
+    evt.preventDefault();
+
+    const target = evt.target;
+
+   if(target.nodeName !== "IMG"){
+    return;
+  }
+  const instance = basicLightbox.create(`<img src="${target}" width="800" height="600"/>`);
+  instance.show();
+  }
+  
+  
